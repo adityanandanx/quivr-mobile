f01468d6e5385f5f46053c08b0dbd15fba28f777
iMADi-ARCH <nandanaditya985@gmail.com>
2023-06-08 16:33:35 +0530

    feat(auth): Logout button

--- pages/dashboard/User/index.tsx
+++ pages/dashboard/User/index.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import PageContainer from "../../../components/PageContainer";
-import { Text } from "react-native-paper";
+import { Button, Text, useTheme } from "react-native-paper";
 import { useSupabase } from "../../../providers/supabase-provider";
 
 interface UserProps {}
 
 const User: FC<UserProps> = ({}) => {
-  const { session } = useSupabase();
+  const { session, supabase } = useSupabase();
+  const theme = useTheme();
 
   return (
     <PageContainer>
@@ -14,6 +15,14 @@ const User: FC<UserProps> = ({}) => {
         Hello {session?.user.email?.split("@")[0]}
       </Text>
       <Text variant="titleMedium">{session?.user.email}</Text>
+      <Button
+        onPress={() => supabase.auth.signOut()}
+        style={{ alignSelf: "flex-start" }}
+        mode="contained-tonal"
+        buttonColor={theme.colors.errorContainer}
+      >
+        Logout
+      </Button>
     </PageContainer>
   );
 };
